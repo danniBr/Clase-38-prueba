b9c801d8e4161f2dd36e6d100a4040de2b56c617
danniBr <danitza.br@gmail.com>
2018-09-08 13:31:48 -0300

    view for task completed

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -2,12 +2,13 @@ class UserTasksController < ApplicationController
 	before_action :authenticate_user!
   def create
   	@task = Task.find(params[:task_id])
-  	@user_tasks = UserTask.new(user: current_user, task: @task)
-    
+  	@user_tasks = UserTask.new(user: current_user, task: @task, status: true)
+
     if @user_tasks.save
-      redirect_to root_path, notice: 'Se ha asignado el proyecto'
+      redirect_to root_path, notice: 'Se ha completado la tarea'
+#      @user_tasks.update_attribute(:status, true)
     else
-      redirect_to root_path, alert: 'No se ha podido asignar'
+      redirect_to root_path, alert: 'No se ha completado la tarea'
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-9.times do | i |
-  Task.create(
-  name: "Comer empanada #{i + 1}", 
+
+Task.create(
+  name: "Comer empanadas", 
   photo: "https://photos.bigoven.com/recipe/hero/beefempanadas-90855e.jpg"
 )
- end
+
+Task.create(
+  name: "Bailar una cueca",
+  photo: "http://img.lasegunda.com/Fotos/2014/09/15/file_20140915084119.jpg"
+)
+
+Task.create(
+  name: "Tomarse un terremoto",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Terremotopiojera.jpg/1280px-Terremotopiojera.jpg"
+)
+
+Task.create(
+  name: "Encumbrar volantin",
+  photo: "http://img.lasegunda.com/Fotos/2012/09/21/file_20120921102411.jpg"
+)
+
+Task.create(
+  name: "Ir a una fonda",
+  photo: "https://nahyarethbugueno.files.wordpress.com/2010/09/fonda26.jpg"
+)
+
+Task.create(
+  name: "Comer choripanes",
+  photo: "https://www.fmdos.cl/wp-content/uploads/2016/09/Choripan-1-250x176.jpg"
+)
+
+Task.create(
+  name: "Tomar mote con huesillo",
+  photo: "https://www.fmdos.cl/wp-content/uploads/2016/09/Mote-con-huesillo-1-1024x683.jpg"
+)
+
+Task.create(
+  name: "Comerse un asado",
+  photo: "https://inspiracionesparatuhogar.cl/wp-content/uploads/2016/08/interiores_imagenes-easy6.jpg"
+)
+
+Task.create(
+  name: "Tirar un trompo",
+  photo: "http://pagina-en-blanco.com/wp-content/uploads/2015/04/Trompos.jpg"
+)
